a55a3421faf6b26eb2bca5267f0bf1fd34d47780
Andressa <146253485+AndresssaSMachado@users.noreply.github.com>
2023-11-21 13:49:23 -0300

    Add files via upload

---
+++ AluraFlix1/_aluraflixprojeto/src/script.js
@@ -0,0 +1,17 @@
+
+var listaFilmes = [] 
+listaFilmes[0] = "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg";
+
+listaFilmes[1] = "https://leiturafilmica.com.br/wp-content/uploads/2017/02/a-chegada-poster.jpg";
+
+listaFilmes[2] = "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg";
+
+listaFilmes[3] = "https://br.web.img3.acsta.net/pictures/20/10/09/10/12/0923276.jpg";
+
+  for (var i = 0; i < 4; i = i + 1) {
+  }
+
+document.write("<img src=" + listaFilmes[0] + ">");
+document.write("<img src=" + listaFilmes[1] + ">");
+document.write("<img src=" + listaFilmes[2] + ">");
+document.write("<img src=" + listaFilmes[3] + ">");
